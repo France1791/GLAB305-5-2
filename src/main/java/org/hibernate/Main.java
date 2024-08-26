43a194e89071dd0b123f1f2d26d37e420ee405bc
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-08-26 10:37:05 -0400

    finished work for lab 305-5-2

---
+++ src/main/java/org/hibernate/Main.java
@@ -0,0 +1,45 @@
+package org.hibernate;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.model.Address;
+import org.hibernate.model.Person;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SessionFactory factory = new Configuration().configure().buildSessionFactory();
+        Session session = factory.openSession();
+
+        Transaction t = session.beginTransaction();
+
+        Address a1 = new Address();
+        a1.setCity("nyc");
+        a1.setState("NY");
+        a1.setStreet("27th street");
+        a1.setZipcode(11103);
+
+        Address a2 = new Address();
+        a2.setCity("Bufflo");
+        a2.setState("NY");
+        a2.setStreet("28th street");
+        a2.setZipcode(15803);
+
+        Person p1 = new Person();
+        p1.setAge(25);
+        p1.setEmail("e.jon@email.com");
+        p1.setName("elbert Adam");
+        p1.setAdress(a1);
+
+        Person p2 = new Person();
+        p2.setAge(45);
+        p2.setEmail("ch.fink@email.com");
+        p2.setName("Christ fink");
+        p2.setAdress(a2);
+
+        session.persist(p1);
+        session.persist(p2);
+        session.persist(a1);
+        session.persist(a2);
+        t.commit();
+    }
+}
